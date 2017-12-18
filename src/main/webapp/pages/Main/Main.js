5a3262ec9ab7771993e93c01ab826e7d2afde121
SaraswathiRekhala <saraswathi.rekhala+11@wavemaker.com>
2017-12-18 06:11:31 +0000

    V1 New files added - [LV_Charts,Page1_1,NewTestDB,WMSTUDIO,hrdb,newtestdbpostgres,data,fonts,lib,vermillion,vermillion] Files modified - [Main,securityService,WEB-INF,profiles,webapp,.wmproject.properties,pom.xml,pages-config.json] Removed files - [fonts,material]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -15,3 +15,17 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
     };
 
 }]);
+
+Application.$controller("DepartmentTable1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("DepartmentLiveForm1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
---
+++ src/main/webapp/pages/Page1_1/Page1_1.js
@@ -0,0 +1,50 @@
+Application.$controller("Page1_1PageController", ["$scope", function ($scope) {
+    "use strict";
+
+    /* perform any action on widgets/variables within this block */
+    $scope.onPageReady = function () {
+        /*
+         * variables can be accessed through '$scope.Variables' property here
+         * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
+         * $scope.Variables.loggedInUser.getData()
+         *
+         * widgets can be accessed through '$scope.Widgets' property here
+         * e.g. to get value of text widget named 'username' use following script
+         * '$scope.Widgets.username.datavalue'
+         */
+    };
+
+}]);
+
+
+
+
+
+
+Application.$controller("AllTypesTableFilter1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("DatetimeTabaleReverseTable1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("AllTypesTableTable1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("AllTypesTableLiveForm1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
